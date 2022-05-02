9875cd843aafff1854b3a7358057ebd4e079ad2f
Karolyne Lima <kmrlima36@gmail.com>
2022-05-02 12:40:05 -0300

    starting api restful 2

---
+++ Arquivo/api/controllers/customerWallets.js
@@ -0,0 +1,31 @@
+const res = require('express/lib/response');
+const uuidv4 = require('uuid/v4');
+
+module.exports = app => {
+    const customerWalletsDB = app.data.customerWallets;
+    const controller = {};
+
+    const{
+      customerWallets: customerWalletsMock,
+    } = customerWalletsDB;
+  
+    controller.listCustomerWallets = (req, res) => res.status(200).json(customerWalletsDB);
+    
+    controller.saveCustomerWallets = (req, res) =>{
+      customerWalletsMock.data.push({
+        id: uuidv4(),
+        parentID: uuidv4(),
+        name: req.body.name,
+        birthDate: req.body.birthDate,
+        cellphone: req.body.cellphone,
+        phone: req.body.phone,
+        email: req.body.email,
+        occupation: req.body.occupation,
+        state: req.body.state
+      });
+    }
+
+    res.status(201).json(customerWalletsMock);
+
+    return controller;
+  }
